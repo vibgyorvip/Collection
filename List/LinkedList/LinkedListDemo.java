d33bcbf97c5e334fa9a612dd4b74591bcc7b4c4b
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2023-08-06 15:16:32 +0530

    Create LinkedListDemo.java
    [Vipul, 30, null, Vipul]
    [CCC, ajeet, Software, 30, null]

---
+++ List/LinkedList/LinkedListDemo.java
@@ -0,0 +1,18 @@
+import java.net.SocketOption;
+import java.util.LinkedList;
+public class LinkedListDemo {
+    public static void main(String[] args)
+    {
+        LinkedList l= new LinkedList();
+        l.add("Vipul");
+        l.add(30);
+        l.add(null);
+        l.add("Vipul");
+        System.out.println(l);//[Vipul, 30, null, Vipul]
+        l.set(0,"Software");//[Software, 30, null, Vipul]
+        l.add(0,"ajeet");//[ajeet, Software, 30, null, Vipul]
+        l.removeLast();     //[ajeet, Software, 30, null]
+        l.addFirst("CCC"); //[CCC, ajeet, Software, 30, null]
+        System.out.println(l);
+    }
+}
