25f3f3ade5448204e93a04d0d5ccb1289442dfe7
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2023-08-20 14:14:55 +0530

    Create HashTableDemo.java
    {6=C, 16=F, 5=A, 15=D, 2=B, 23=E}

---
+++ Map/HashTable/HashTableDemo.java
@@ -0,0 +1,35 @@
+import java.util.Hashtable;
+
+public class HashTableDemo {
+    public static void main(String[] args)
+    {
+        Hashtable h = new Hashtable();
+        h.put(new Test(5),"A");
+        h.put(new Test(2),"B");
+        h.put(new Test(6),"C");
+        h.put(new Test(15),"D");
+        h.put(new Test(23),"E");
+        h.put(new Test(16),"F");
+        //h.put("durga",null);//NullPointerException
+        System.out.println(h);//{6=C, 16=F, 5=A, 15=D, 2=B, 23=E} based on hashcode
+    }
+}
+
+class Test
+{
+    int i;
+    Test(int i)
+    {
+        this.i =i;
+    }
+
+    @Override
+    public int hashCode() {
+        return i;
+    }
+
+    @Override
+    public String toString() {
+        return i+"";
+    }
+}
