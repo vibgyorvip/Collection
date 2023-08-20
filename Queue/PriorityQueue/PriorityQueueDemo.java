5b86c2011f14b3d74e49f512c742860d65b05fab
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2023-08-20 18:31:36 +0530

    Create PriorityQueueDemo.java
    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
    0
    [1, 3, 2, 7, 4, 5, 6, 10, 8, 9]

---
+++ Queue/PriorityQueue/PriorityQueueDemo.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+    public static void main(String[] args)
+    {
+        PriorityQueue q = new PriorityQueue();
+        //System.out.println(q.peek()); //null
+        //System.out.println(q.element());//R.T.E : NoSuchElementException
+
+        for(int i=0; i<=10; i++)
+        {
+            q.offer(i);
+        }
+        System.out.println(q);//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        System.out.println(q.poll());//0 (removed)
+
+        System.out.println(q);//[1, 3, 2, 7, 4, 5, 6, 10, 8, 9]
+    }
+}
